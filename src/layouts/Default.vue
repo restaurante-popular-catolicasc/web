cb0b3f71a3272cdfb668a885070d2c5c528d74fc
Luis Felipe Manfroni <manfroni.luis@gmail.com>
2019-09-19 19:35:53 -0300

    Adicionado layouts de páginas padrão

---
+++ src/layouts/Default.vue
@@ -0,0 +1,29 @@
+<template>
+  <VApp>
+    <VAppBar app>
+      <VToolbarTitle class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </VToolbarTitle>
+      <VSpacer />
+      <VBtn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+        <VIcon>mdi-open-in-new</VIcon>
+      </VBtn>
+    </VAppBar>
+
+    <VContent>
+      <RouterView />
+    </VContent>
+  </VApp>
+</template>
+
+<script>
+export default {
+  name: 'Default'
+}
+</script>
